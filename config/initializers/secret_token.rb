5ce9ecd27c3bfe0621140bbb7ff329f062007afe
kotohata <t.kotohata@gmail.com>
2012-10-19 22:45:20 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bungaku::Application.config.secret_token = '2963f11e2e575393910fe4edf39c60a25d6c67ce62c45c8b8f0726bfd86578051b5d0b95d3e28e99a3a5c3662d824128a17e573ae22e2348ea66d65b6dacb17c'

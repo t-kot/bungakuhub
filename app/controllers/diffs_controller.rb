32c390913378f7fc09094d96d2e769ccafd54e7b
kotohata-imac <t.kotohata@gmail.com>
2012-12-21 18:49:21 +0900

    add diff error if no diff object found

--- app/controllers/diffs_controller.rb
+++ app/controllers/diffs_controller.rb
@@ -4,6 +4,8 @@ class DiffsController < ApplicationController
     @kommit1 = Kommit.find(params[:kommit_id])
     @kommit2 = Kommit.find(params[:id])
     @repository = @kommit1.repository
+    @diffs = @repository.repo.diff(@kommit1.revision, @kommit2.revision)
+    raise BungakuHub::GitError.new("no diff found") if @diffs.blank?
     render "/kommits/diff/"
   end
 
